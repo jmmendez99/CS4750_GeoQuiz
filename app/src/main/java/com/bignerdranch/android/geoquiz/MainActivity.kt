eab6ffeedb8f582d839e00d80538baad9bfbb286
Justin Mendez <jmmendez@cpp.edu>
2022-09-13 14:23:47 -0700

    Progress on Chapter 6's second activity

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.geoquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
     //Add a lazily initialized property to stash the QuizViewModel
@@ -42,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         //Setting the listeners for true and false button clicks.
@@ -59,6 +62,13 @@ class MainActivity : AppCompatActivity() {
             quizViewModel.moveToNext()
             updateQuestion()
         }
+
+        cheatButton.setOnClickListener {
+            //Start Cheat activity and also is a button click listener
+            val intent = Intent(this, CheatActivity::class.java)
+            startActivity(intent)
+        }
+
         updateQuestion()
     }
 
